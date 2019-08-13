088e4fb8247a0a283d42b76559ceec537426dfff
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-13 14:11:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,14 +1,16 @@
+require "pry"
+binding.pry
 
 def run_guessing_game
  rand(6) + 1
  lucky_number = rand(6) +1
- puts "Please enter your number"
-user_inputs = gets.chomp
-if user_inputs == 'exit'
-puts 'Goodbye'
-elsif user_inputs == lucky_number
- puts "You guessed the correct number!"
- elsif user_inputs != lucky_number
- puts "Sorry! The computer guessed #{lucky_number}."
+  user_input = gets.chomp
+  binding.pry
+  if user_input == lucky_number
+    puts "You guessed the correct number!"
+    elsif user_input != lucky_number
+      puts "Sorry! The computer guessed #{lucky_number}."
+      elsif user_input == "exit"
+      puts "Goodbye!"
   end
 end
