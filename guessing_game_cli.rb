58ebca375efb14b8b0402f59521cbc5126cbfd18
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-13 14:15:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,11 @@
 require "pry"
-binding.pry
+#binding.pry
 
 def run_guessing_game
  rand(6) + 1
  lucky_number = rand(6) +1
   user_input = gets.chomp
-  binding.pry
+ # binding.pry
   if user_input.to_i == lucky_number
     puts "You guessed the correct number!"
     elsif user_input.to_i != lucky_number
