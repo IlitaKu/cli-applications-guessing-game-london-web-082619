13c39b7915193fa01004d4714fb3edbeda0c0ad7
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-13 14:09:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
 
+def run_guessing_game
+ rand(6) + 1
+ lucky_number = rand(6) +1
+ puts "Please enter your number"
+user_inputs = gets.chomp
+if user_inputs == 'exit'
+puts 'Goodbye'
+elsif user_inputs == lucky_number
+ puts "You guessed the correct number!"
+ elsif user_inputs != lucky_number
+ puts "Sorry! The computer guessed #{lucky_number}."
+  end
+end
